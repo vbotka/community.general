0fcb91af1dcef3212b096228e07b79d4fcdbea06
Vladimir Botka <vbotka@gmail.com>
2020-12-30 12:35:14 +0100

    Added filter items2dict2

--- plugins/filter/list.py
+++ plugins/filter/list.py
@@ -226,6 +226,54 @@ def any2items(x, key='key', override=False):
         return l
 
 
+def items2dict2(mylist, key_name='key', value_name='value', default_value=None):
+    '''Takes a list of dicts with each having a 'key' and 'value' keys,
+    and transforms the list into a dictionary, effectively as the
+    reverse of dict2items. If 'value_name' does not exist use
+    'default_value'.
+
+    - name: Example 1
+      debug:
+        msg: "{{ fruits|items2dict2 }}"
+      vars:
+        fruits:
+          - key: apple
+            value: green
+          - key: banana
+          - key: orange
+
+      msg:
+        apple: green
+        banana: null
+        orange: null
+
+    - name: Example 2
+      debug:
+        msg: "{{ fruits|items2dict2(key_name='k',
+                                    value_name='v',
+                                    default_value='undefined') }}"
+      vars:
+        fruits:
+          - k: apple
+            v: green
+          - k: banana
+          - k: orange
+
+       gives:
+
+       msg:
+         apple: green
+         banana: undefined
+         orange: undefined
+    '''
+
+    if not isinstance(mylist, Sequence):
+        raise AnsibleFilterError("First argument for community.general.items2dict2 requires a list. %s is %s" %
+                                 (mylist, type(mylist)))
+
+    return dict((item[key_name], item.setdefault(value_name, default_value)) for item in mylist)
+
+
 class FilterModule(object):
     ''' Ansible list filters '''
 
@@ -233,4 +281,5 @@ class FilterModule(object):
         return {
             'lists_mergeby': lists_mergeby,
             'any2items': any2items,
+            'items2dict2': items2dict2,
         }
